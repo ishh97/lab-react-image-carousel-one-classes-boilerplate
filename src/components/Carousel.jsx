3907905539e4a116372dd23b7a9f77781a69270b
ishh97 <142491547+ishh97@users.noreply.github.com>
2023-12-14 11:43:28 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,47 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+  increase = () => {
+    if (this.state.count < 2) {
+      this.setState({ count: this.state.count += 1 })
+    }
+  }
+  decrease = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count -= 1 })
+    }
+  }
+  render() {
+    console.log(images);
+    return (
+      <div>
+        <div className="main">
+          <button className="backwardArrow" onClick={this.decrease}>
+            <ArrowBackIosIcon />
+          </button>
+          <img src={images[this.state.count].img} alt="" />
+
+          <button className="forwardArrow" onClick={this.increase}>
+            <ArrowForwardIosIcon />
+          </button>
+        </div>
+        <div className="title">
+          <h1>{images[this.state.count].title}</h1>
+        </div>
+        <div className="subtitle">
+          <p>{images[this.state.count].subtitle}</p>
+        </div>
+      </div>
+
+    )
+  }
+
 }
 
 export default Carousel;
